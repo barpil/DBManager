0ec3475fedc1a64b276859e69c3605b7c766c653
barpil <bartosz.pilat4@gmail.com>
2024-07-15 20:06:32 +0200

    Przekształcono projekt na projekt Maven oraz utworzono działający plik JAR

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        uruchom();
+    }
+
+    private static void uruchom(){
+        OknoGlowne.getOknoGlowne();
+        OknoWczytywaniaBazyDanych.showOknoWczytywania();
+
+    }
+}
